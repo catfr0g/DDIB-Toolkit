c252d84b9ef03eb0e30a37a47a2dfbd497961b41
Mikhail Zimin <m.zimin@innopolis.university>
2025-11-25 18:42:29 +0300

    module ptah change

--- ddib/src/experiments/dataset.py
+++ ddib/src/experiments/dataset.py
@@ -4,7 +4,7 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from ddib.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from experiments.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 app = typer.Typer()
 
